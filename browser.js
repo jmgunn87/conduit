1942f4bd37a9b4a8b9c866d5c4c6352500e217f8
James Marshall-Gunn <jmgunn87@gmail.com>
2014-06-15 20:08:32 +0100

    reformatting of exports

--- browser.js
+++ browser.js
@@ -1,16 +1,10 @@
-var Model = require('./src/model');
-var Container = require('./src/container');
-var Validator = require('./src/validator');
-var Transcoder = require('./src/transcoder');
-var Mapper = require('./src/mapper');
-var Adapter = require('./src/adapter');
 var Conduit = require('./src/conduit');
 
-Conduit.Model = Model;
-Conduit.Container = Container;
-Conduit.Validator = Validator;
-Conduit.Transcoder = Transcoder;
-Conduit.Mapper = Mapper;
-Conduit.Adapter = Adapter;
+Conduit.Model = require('./src/model');
+Conduit.Container = require('./src/container');
+Conduit.Validator = require('./src/validator');
+Conduit.Transcoder = require('./src/transcoder');
+Conduit.Mapper = require('./src/mapper');
+Conduit.Adapter = require('./src/adapter');
 
 module.exports = Conduit;
--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-var Model = require('./src/model');
-var Container = require('./src/container');
-var Validator = require('./src/validator');
-var Transcoder = require('./src/transcoder');
-var Mapper = require('./src/mapper');
-var Adapter = require('./src/adapter');
-var LevelDBAdapter = require('./src/leveldb-adapter');
-var SQLite3Adapter = require('./src/sqlite3-adapter');
-var RestJsonAdapter = require('./src/rest-json-adapter');
-var ZMQAdapter = require('./src/zmq-adapter');
 var Conduit = require('./src/conduit');
 
-Conduit.Model = Model;
-Conduit.Container = Container;
-Conduit.Validator = Validator;
-Conduit.Transcoder = Transcoder;
-Conduit.Mapper = Mapper;
-Conduit.Adapter = Adapter;
-Conduit.LevelDBAdapter = LevelDBAdapter;
-Conduit.SQLite3Adapter = SQLite3Adapter;
-Conduit.RestJsonAdapter = RestJsonAdapter;
-Conduit.ZMQAdapter = ZMQAdapter;
+Conduit.Model = require('./src/model');
+Conduit.Container = require('./src/container');
+Conduit.Validator = require('./src/validator');
+Conduit.Transcoder = require('./src/transcoder');
+Conduit.Mapper = require('./src/mapper');
+Conduit.Adapter = require('./src/adapter');
+Conduit.LevelDBAdapter = require('./src/leveldb-adapter');
+Conduit.SQLite3Adapter = require('./src/sqlite3-adapter');
+Conduit.RestJsonAdapter = require('./src/rest-json-adapter');
+Conduit.ZMQAdapter = require('./src/zmq-adapter');
 
 module.exports = Conduit;
