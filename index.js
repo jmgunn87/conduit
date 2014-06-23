18ac3defb35777a65aa3923b83a4c34e5085aa8c
James Marshall-Gunn <jmgunn87@gmail.com>
2014-06-23 03:13:01 +0100

    0.1.3

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ Conduit.Transcoder = require('./src/transcoder');
 Conduit.Mapper = require('./src/mapper');
 Conduit.Adapter = require('./src/adapter');
 Conduit.LevelDBAdapter = require('./src/leveldb-adapter');
+Conduit.MongoDBAdapter = require('./src/mongodb-adapter');
 Conduit.SQLite3Adapter = require('./src/sqlite3-adapter');
 Conduit.RestJsonAdapter = require('./src/rest-json-adapter');
 Conduit.ZMQAdapter = require('./src/zmq-adapter');
